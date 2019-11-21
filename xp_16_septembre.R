2b1a4f406febd5ab2df90afe19d694205489690a
defuneste <leroy.oli@gmail.com>
2019-11-21 11:14:58 +0100

    un seul fichier xp et variables en bon format

--- xp_16_septembre.R
+++ xp_16_septembre.R
@@ -1,7 +1,7 @@
 ##.###################################################################################33
 ## I. Chargement des données de l'xp ====
 ##.#################################################################################33
-ha
+
 ## 1 - les données ================
 
 # ici les données nous sont arrivées par bout et pas toujours sous la même forme ce qui explique le 
@@ -29,24 +29,44 @@ dim(xp_01_10_bota.shp)
 ## II. une seule db ====
 ##.#################################################################################33
 
+
+## 1 - On fusione ================
+
 # on vérifie nom et ordres des colonnes
 names(xp_bota.shp) == names(xp_16_09_bota.shp)
 names(xp_bota.shp) == names(xp_01_10_bota.shp)
 
 # on fusionne le tout
-# ici je suis resté en Rbase pe faire en dplyr pour ne pas perdre la géométrie
+# ici je suis resté en Rbase pe nfaire en dplyr pour ne pas perdre la géométrie
 xp.dat <- rbind(as.data.frame(xp_16_09_bota.shp), as.data.frame(xp_bota.shp), as.data.frame(xp_01_10_bota.shp))
+
+# on en refait un objet sf
+xp.shp <- st_as_sf(xp.dat, sf_column_name = "geometry")
+
+## 2 - On corrige/armonise les données ================
 
 # on vérifie 
 summary(xp.shp)
 dim(xp.shp)
 
-# on en refait un objet sf
-xp.shp <- st_as_sf(xp.dat, sf_column_name = "geometry")
+# on repasse en factor / num pour les participants
+xp.shp <- transform(xp.shp,Participant = as.factor(as.numeric(factor(username))))
 
+# correction d'étrangeté : un oubli de bon !  
+# je l'ai directement corrigé dans le json maintenant il y a des "vides" qui devrait 
+# févier d''amerique à verifier
 
-xp.shp[xp.shp$genus == "Platanus",]
+unique(xp.shp$confiance)
 
+# on passe en facteur pour confiance
+xp.shp$confiance <- factor(xp.shp$confiance)
+
+#ajout en facteur
+xp.shp$ajout[is.na(xp.shp$ajout)] <- 0
+xp.shp$ajout <- factor(xp.shp$ajout)
+
+#verif
+summary(xp.shp)
 
 ##.###################################################################################33
 ## III. Première stats ====
