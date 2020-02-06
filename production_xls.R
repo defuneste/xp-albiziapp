2e6628b577de4b661f919b041780ebf8211b1b51
defuneste <leroy.oli@gmail.com>
2020-02-06 12:15:49 +0100

    rm de fichier temp et rajout documentation

--- production_xls.R
+++ production_xls.R
@@ -2,11 +2,6 @@
 ## I. Petit script pour faire de l'xls ====
 ##.#################################################################################33
 
-# on charge les données de l'xp
-#source("chargement_xp.R")
-# source("xp_16_septembre.R")
-source("chargement_xp_01_10.R")
-
 # il y a d'autre packages pour faire du xls
 # xlsx demande java jdk
 # XLconnect est dit un peu plus lent
@@ -15,12 +10,13 @@ source("chargement_xp_01_10.R")
 library(openxlsx)
 
 # il faut séclectionner le bon fichier 
-
-xp_bota.shp <- xp.shp
+xp_bota.shp <- xp_total.shp
 
 xp_bot.df <- as.data.frame(xp_bota.shp) # le tibe est moins bien supporté que le df
 xp_bot.df <- subset(xp_bot.df,select = -geometry) # xls aime pas le champ géométrie
 list_xp_bot.df <- split(xp_bot.df, xp_bot.df$username) # verifier que username est un facteur
 
 # mettre le nom correspondant
-write.xlsx(file = "xp_total.xlsx", list_xp_bot.df)
+write.xlsx(file = "data/xp_total.xlsx", list_xp_bot.df)
+
+rm(list_xp_bot.df, xp_bot.df, xp_bota.shp)
